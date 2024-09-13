380b54416077acce342d59d6e7d1203b669846ce
Vitaliy <vitaliy.matvienko.prof@gmail.com>
2024-09-13 17:12:39 +0400

    fix layout auth for hooks.server

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,48 +1,20 @@
 <script>
     import "../app.css";
-    import { createBrowserClient, createServerClient, isBrowser } from '@supabase/ssr'
-    import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public'
-    import type { LayoutLoad } from './$types'
+    import { invalidate } from '$app/navigation';
+    import { onMount } from 'svelte';
 
-    export const load: LayoutLoad = async ({ data, depends, fetch }) => {
-        /**
-         * Declare a dependency so the layout can be invalidated, for example, on
-         * session refresh.
-         */
-        depends('supabase:auth')
+    export let data;
+    $: ({ session, supabase } = data);
 
-        const supabase = isBrowser()
-            ? createBrowserClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
-                global: {
-                    fetch,
-                },
-            })
-            : createServerClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
-                global: {
-                    fetch,
-                },
-                cookies: {
-                    getAll() {
-                        return data.cookies
-                    },
-                },
-            })
+    onMount(() => {
+        const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+            if (newSession?.expires_at !== session?.expires_at) {
+                invalidate('supabase:auth');
+            }
+        });
 
-        /**
-         * It's fine to use `getSession` here, because on the client, `getSession` is
-         * safe, and on the server, it reads `session` from the `LayoutData`, which
-         * safely checked the session using `safeGetSession`.
-         */
-        const {
-            data: { session },
-        } = await supabase.auth.getSession()
-
-        const {
-            data: { user },
-        } = await supabase.auth.getUser()
-
-        return { session, supabase, user }
-    }
+        return () => data.subscription.unsubscribe();
+    });
 
 </script>
 
